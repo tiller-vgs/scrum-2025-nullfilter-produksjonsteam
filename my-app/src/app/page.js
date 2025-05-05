46ac91370707c0ae1a887cd1004b44934a49c111
martin <mskirsebom@gmail.com>
2025-05-05 09:44:45 +0200

    aoutcomplete = off

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -3,28 +3,28 @@ import React from "react";
 function Page() {
   return (
     <div>
-      <form action="/dashboard">
-        <label htmlFor="uname">Username:</label>
-        <br />
-        <input
-          className="bg-gray-200 outline-black outline-1"
-          type="text"
-          id="uname"
-          name="uname"
-        />
-        <br />
-        <label htmlFor="password">Password:</label>
-        <br />
-        <input
-          className="bg-gray-200 outline-black outline-1"
-          type="text"
-          id="password"
-          name="password"
-        />
-        <br />
-        <br />
-        <input className="bg-blue-100" type="submit" value="Submit" />
-      </form>
+      <label htmlFor="uname">Username:</label>
+      <br />
+      <input
+        className="bg-gray-200 outline-black outline-1"
+        type="text"
+        id="uname"
+        name="uname"
+        autoComplete="off"
+      />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <br />
+      <input
+        className="bg-gray-200 outline-black outline-1"
+        type="text"
+        id="password"
+        name="password"
+        autoComplete="off"
+      />
+      <br />
+      <br />
+      <input className="bg-blue-100" type="submit" value="Submit" />
     </div>
   );
 }
